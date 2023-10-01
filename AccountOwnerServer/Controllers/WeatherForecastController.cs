48b9eb659e2f58e8098fc903926a199fcb095ad7
robin-kumar <robinkumar27@gmail.com>
2023-10-01 18:56:02 +0530

    NLog implementation

--- AccountOwnerServer/Controllers/WeatherForecastController.cs
+++ AccountOwnerServer/Controllers/WeatherForecastController.cs
@@ -1,3 +1,4 @@
+using Contracts;
 using Microsoft.AspNetCore.Mvc;
 
 namespace AccountOwnerServer.Controllers
@@ -11,23 +12,21 @@ namespace AccountOwnerServer.Controllers
         "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
     };
 
-        private readonly ILogger<WeatherForecastController> _logger;
+        private readonly ILoggerManager _logger;
 
-        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        public WeatherForecastController(ILoggerManager logger)
         {
             _logger = logger;
         }
 
         [HttpGet]
-        public IEnumerable<WeatherForecast> Get()
+        public IEnumerable<string> Get()
         {
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-            {
-                Date = DateTime.Now.AddDays(index),
-                TemperatureC = Random.Shared.Next(-20, 55),
-                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-            })
-            .ToArray();
+            _logger.LogInfo("Here is info message from the controller.");
+            _logger.LogDebug("Here is debug message from the controller.");
+            _logger.LogWarn("Here is warn message from the controller.");
+            _logger.LogError("Here is error message from the controller.");
+            return new string[] { "value1", "value2" };
         }
     }
 }
--- AccountOwnerServer/Extensions/ServiceExtensions.cs
+++ AccountOwnerServer/Extensions/ServiceExtensions.cs
@@ -1,4 +1,7 @@
-﻿namespace AccountOwnerServer.Extensions
+﻿using Contracts;
+using LoggerService;
+
+namespace AccountOwnerServer.Extensions
 {
     public static class ServiceExtensions
     {
@@ -18,6 +21,11 @@
             {
 
             });
+        }
+
+        public static void ConfigureLoggerService(this IServiceCollection services)
+        {
+            services.AddSingleton<ILoggerManager, LoggerManager>();
         }
     }
 }
